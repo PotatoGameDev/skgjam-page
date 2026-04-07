66af40dd838d42fad63e2392124c02c89fa738ee
potato <potato@potatogam.es>
2026-04-08 01:23:22 +0200

    Added scores to the jam data, also added jam scraper script

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Heading
+# SKG Community Jam Web Page
+
+Simple page for the SKG Community Jam.
+
+## Data Preparation
+In src/_data there is a python script that scrapes itch.io for all the data.
+Initialize venv:
+
+```bash
+cd src/_data
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Run:
+```bash
+python3 data-collector.py -j 12345 -o ./skgjam202Xentries.json
+```
+Obviously substitute 12345 and X with something.
+the -j param you can scrape from the source of the jam page.
+It's basically the internal itch.io jam id.
+
+The file name will be the name of data variable in eleventy.
+It will contain the entry details and the scores for all the entries.
+
+
+
+
---
+++ data-collector/data-collector.py
@@ -0,0 +1,82 @@
+import json
+import requests
+import time
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Scrape itch.io jam results and enrich with the results"
+    )
+    # Example:
+    # https://itch.io/jam/407781/entries.json
+    parser.add_argument(
+        "-j", "--jam",
+        default="407781",
+        help="Id of the game jam, you have to manually figure it out by inspecting itch jam page's code for now... Defaults to the original 2025 jam id."
+    )
+    parser.add_argument(
+        "-o", "--output",
+        default="jam_games_2025.json",
+        help="Name for the output file."
+    )
+    return parser.parse_args()
+
+def get_entries(args):
+    jam_entries_response = requests.get(f"https://itch.io/jam/{args.jam}/entries.json") 
+    jam_entries_response.raise_for_status()
+
+    return jam_entries_response.json()
+
+def get_entry_page(game_url):
+    full_url = "https://itch.io/" + game_url
+
+    html_response = requests.get(full_url)
+    html_response.raise_for_status()
+
+    return html_response
+
+def main():
+    args = parse_args()
+
+    entries = get_entries(args)
+
+    for entry in tqdm(entries["jam_games"], desc="Downloading jam results"):
+        entry_html = get_entry_page(entry["url"])
+
+        soup = BeautifulSoup(entry_html.text, "html.parser")
+
+        table = soup.find("table", class_="ranking_results_table")
+
+        rows = table.find_all("tr")
+
+        rows = rows[1:] # removing the header
+
+        scores = {}
+
+        for row in rows:
+            cells = row.find_all("td")
+            criteria = cells[0].get_text(strip=True)
+            rank = cells[1].get_text(strip=True)
+            rank_num = int(rank.replace("#", ""))
+            score = float(cells[2].get_text(strip=True))
+            raw_score = float(cells[3].get_text(strip=True))
+
+            scores[criteria] = {
+                    "rank": rank_num,
+                    "score": score,
+                    "raw_score": raw_score,
+            }
+
+        entry["scores"] = scores
+
+        time.sleep(0.5)
+
+        
+    with open(args.output, "w") as f:
+        json.dump(entries, f)
+
+if __name__ == "__main__":
+    main()
