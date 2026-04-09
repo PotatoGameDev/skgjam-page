a2ff1892f3b889f9fa5f52206b3e0b10a4d18f23
potato <potato@potatogam.es>
2026-04-09 21:58:06 +0200

    Trying to fix css routing

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  eleventyConfig.addFilter("toBaseUrl", (path) => {
+    const base = process.env.ELEVENTY_BASE_URL;
+    if (!base) return path;
+    return path.startsWith("/") ? base + path : path;
+  });
+
   return {
     dir: {
       input: "src",
