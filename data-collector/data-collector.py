94987919e52455bbdb8e318a82d650d9fd385541
potato <potato@potatogam.es>
2026-04-09 02:38:18 +0200

    Added css, showreel, some fixes

--- data-collector/data-collector.py
+++ data-collector/data-collector.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 import time
 import argparse
@@ -18,9 +19,9 @@ def parse_args() -> argparse.Namespace:
         help="Id of the game jam, you have to manually figure it out by inspecting itch jam page's code for now... Defaults to the original 2025 jam id."
     )
     parser.add_argument(
-        "-o", "--output",
-        default="jam_games_2025.json",
-        help="Name for the output file."
+        "-n", "--output-name",
+        default="jam_games_2025",
+        help="The output base name"
     )
     return parser.parse_args()
 
@@ -38,13 +39,32 @@ def get_entry_page(game_url):
 
     return html_response
 
+def save_cover(entry, output_dir):
+    # Saving the cover
+    cover_url = entry["game"]["cover"]
+    if cover_url:
+        cover_response = requests.get(cover_url)
+        cover_response.raise_for_status()
+
+        _, ext = os.path.splitext(cover_url)
+
+        # writing the entry
+        with open(output_dir + "/" + str(entry["id"]) + "_cover" + ext, "wb") as f:
+            f.write(cover_response.content)
+
 def main():
     args = parse_args()
 
     entries = get_entries(args)
 
+    output_dir = "../src/_data/" + args.output_name
+    os.makedirs(output_dir, exist_ok=True)
+
     for entry in tqdm(entries["jam_games"], desc="Downloading jam results"):
         entry_html = get_entry_page(entry["url"])
+
+        # Saving of the covers probably is not a good idea.
+        # save_cover(entry, output_dir)
 
         soup = BeautifulSoup(entry_html.text, "html.parser")
 
@@ -74,9 +94,9 @@ def main():
 
         time.sleep(0.5)
 
-        
-    with open(args.output, "w") as f:
+    with open(output_dir + "/jam_games.json", "w") as f:
         json.dump(entries, f)
+
 
 if __name__ == "__main__":
     main()
--- src/skgjam2025.md
+++ src/skgjam2025.md
@@ -2,11 +2,13 @@
 title: Stop Killing Games Community Jam (2025)
 layout: layouts/base.njk
 permalink: /skgjam2025/
+pageStyle: skgjam2025.css
+jamLogo: skgjam2025.png
 ---
 
 # Stop Killing Games Community Jam
 
-This is the first SKG Community Jam, conducted in July of 2025, the hot month of the final
+This was the first SKG Community Jam, conducted in July of 2025, the hot month of the final
 make or break of SKG movement, when we tried to do everything to push that petition
 progress bar forward, and some of us made games for the jam!
 
@@ -20,4 +22,5 @@ and everyone had fun for a good cause :)
 The jam took 10 days, and 7 days for community judging.
 In the last day, August 1st, the final results have been presented in the form of this cool showreel:
 
-[TODO: Showreel]
+
+<iframe width="100%" height="562" src="https://www.youtube.com/embed/cKZJm0mN2Oc" title="SKG Game Jam Showreel" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
